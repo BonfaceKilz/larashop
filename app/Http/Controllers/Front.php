f4229e951d27bf2ff1af2925f031d98486103f3d
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-01-07 10:36:25 +0300

    initial commit

---
+++ app/Http/Controllers/Front.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Front extends Controller
+{
+    public function index(){
+        return view('home', ['page'=>'home']);
+    }
+
+    public function products(){
+        return view('products', ['page'=>'products']);
+    }
+
+    public function product_details($id){
+        return view('product_details', ['page'=>'products']);
+    }
+
+    public function product_categories(){
+        return view('products', ['page'=>'products']);
+    }
+
+    public function product_brands(){
+        return view('products', ['page'=>'products']);
+    }
+
+    public function  blog(){
+        return view('blog', ['page'=>'blog']);
+    }
+
+    public function blog_post($id){
+        return view('blog_post', ['page'=>'blog']);
+    }
+
+    public function contact_us(){
+        return view('contact_us', ['page'=>'contact_us']);
+    }
+
+    public function login(){
+        return view('login', ['page'=>'home']);
+    }
+    
+    public function logout(){
+        return view('login', ['page'=>'home']);
+    }
+
+    public function cart(){
+        return view('cart', ['page'=>'home']);
+    }
+
+    public function checkout(){
+        return view('checkout', ['page'=>'home']);
+    }
+
+    public function search($query){
+        return view('products', ['page'=>'products']);
+    }
+}
